77751f9cd7fe63d98818cdaf2d53a20c6e1c3c2b
Siddheshwar <ssanap110@gmail.com>
2024-12-25 16:46:28 +0530

    JWT Added in Journal Application

--- src/main/java/net/engineeringdigest/journalApp/controller/PublicController.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/PublicController.java
@@ -1,25 +1,40 @@
 package net.engineeringdigest.journalApp.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import net.engineeringdigest.journalApp.entity.User;
+import net.engineeringdigest.journalApp.service.UserDetailsServiceImpl;
 import net.engineeringdigest.journalApp.service.UserService;
+import net.engineeringdigest.journalApp.utilis.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 @RequestMapping("/public")
 public class PublicController {
 
+    @Autowired
+    private  UserService userService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
 
     @Autowired
-    UserService userService;
+    private JwtUtil jwtUtil;
 
-    @PostMapping("/create-user")
-    public ResponseEntity<User> createEntry(@RequestBody User myEntry){
+
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<User> signUp(@RequestBody User myEntry){
         try{
             userService.saveNewUser(myEntry);
             return  new ResponseEntity<>(myEntry, HttpStatus.CREATED);
@@ -28,5 +43,21 @@ public class PublicController {
         }
 
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user){
+        try{
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUserName(),user.getPassword()));
+            UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUserName());
+            String jwt = jwtUtil.generateToken(userDetails.getUsername());
+            return new ResponseEntity<>(jwt,HttpStatus.OK);
+        }catch (Exception e){
+            log.error("Exception occured while create authentication token",e);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+
 
 }
